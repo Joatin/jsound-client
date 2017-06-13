a0bd44bd54db4fd104ea3a42c0a3414f4f6d6f6a
Joatin Granlund <granlundjoatin@icloud.com>
2017-06-13 23:35:51 +0200

    Fix for minification

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,5 +3,6 @@ import { Observable } from 'rxjs';
 export abstract class AuthService {
   public abstract login(): void;
   public abstract logout(): Observable<void>;
-  public abstract isAuthenticated(): boolean
+  public abstract isAuthenticated(): boolean;
+  public abstract handleAuthentication(): void;
 }
